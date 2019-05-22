4f0ae951a2415cf8759bc66390a575687d1fe7e0
Joshkinz <jdeastin02@gmail.com>
2019-05-22 10:49:15 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -3,40 +3,41 @@ class Names:
   nameList = ["arq", "fffafnir", "phantom", "devinthunder", "kinghaynes", "ultramario1998", "kyler", "joshkinz", "coolprez", "asian trash", "crying", "jamsterbuggy", "qlonever", "fiji", "artlessuser", "swiggle", "shadowjoe323", "maxop333"]
   
   def nameCheck(name):
-			  name = name.lower()
-			  if name not in nameList:
-			  	if name == "nfan":
-			  		return "arq"
-			  	if name == "marc" or "fffaf":
-			  		return "fffafnir"
-			  	if name == "dex":
-			  		return "phantom"
-			  	if name == "devin":
-			  		return "devinthunder"
-			  	if name == "haynes":
-			  		return "kinghaynes"
-			  	if name == "ultramario1998":
-			  		return "mario"
-			  	if name == "josh":
-			  		return "joshkinz"
-			  	if name == "prez":
-			  		return "coolprez"
-			  	if name == "asian" or "yoon":
-			  		return "asian trash"
-			  	if name == "sam":
-			  		return "crying"
-			  	if name == "jam" or "jamster":
-			  		return "jamsterbuggy"
-				if name == "qlon":
-					return "qlonever"
-				if name == "artless" or "artles":
-					return "artlessUser"
-				if name == "swig":
-					return "swiggle"
-				if name == "joe":
-					return "shadowjoe323"
-				if name == "max":
-					return "maxop333"
-			if name in nameList:
-				return name
+	name = name.lower()
+	if name not in nameList:
+		if name == "nfan":
+			return "arq"
+		if name == "marc" or "fffaf":
+	  		return "fffafnir"
+	 	if name == "dex":
+			return "phantom"
+		if name == "devin":
+			  return "devinthunder"
+		if name == "haynes":
+	 		return "kinghaynes"
+		if name == "ultramario1998":
+	  		return "mario"
+		if name == "josh":
+			return "joshkinz"
+ 		if name == "prez":
+	  		return "coolprez"
+ 		if name == "asian" or "yoon":
+	  		return "asian trash"
+ 		if name == "sam":
+	 		return "crying"
+		if name == "jam" or "jamster":
+	  		return "jamsterbuggy"
+		if name == "qlon":
+			return "qlonever"
+		if name == "artless" or "artles":
+			return "artlessUser"
+		if name == "swig":
+			return "swiggle"
+		if name == "joe":
+			return "shadowjoe323"
+		if name == "max":
+			return "maxop333"
+		
+	if name in nameList:
+		return name
                   
